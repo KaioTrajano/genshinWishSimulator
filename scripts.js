ac4675853acd6ce69270b47032f5c4d20ed9e151
Kaio <78030357+KaioTrajano@users.noreply.github.com>
2022-03-25 21:11:52 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -372,8 +372,8 @@ var images = [
     item10.style.backgroundImage = "url('epicbackground.svg')" : item10.style.backgroundImage = "url('itembackground.svg')"
         
         
-        pity5 = 0
-         pity5estrelas.innerHTML=  "Pity 5 estrelas: " + pity5
+        
+        
         
     
     item1.getAttribute('src') == "zhongli.svg" | item2.getAttribute('src') == "zhongli.svg" | 
@@ -400,7 +400,7 @@ var images = [
        
         
     
-    
+     pity5estrelas.innerHTML=  "Pity 5 estrelas: " + pity5
     
     pity = 0
     
